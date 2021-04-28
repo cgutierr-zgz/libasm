3c0c69c80ad07fc086bcc1fc6f1b1d078a210158
Carlos <carlosgutimo.3d@gmail.com>
2021-04-28 22:22:44 +0200

    28/04/2021 22:22

--- main.c
+++ main.c
@@ -6,51 +6,48 @@
 /*   By: cgutierr <cgutierr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 19:03:36 by cgutierr          #+#    #+#             */
-/*   Updated: 2021/04/28 22:14:36 by cgutierr         ###   ########.fr       */
+/*   Updated: 2021/04/28 22:22:42 by cgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 
 /*
-COMPILAR -> nasm -f macho64 ft_strlen.s
-gcc main.c -c
-cc main.o ft_strlen.o
-./a.out
-
-nasm -f macho64 a todas las funciones
-nasm -f macho64 ft_write.s && gcc main.c -c && gcc main.o ft_strlen.o ft_write.o && ./a.out
+**	Compilar:
+**		nasm -f macho64 ft_strlen.s && nasm -f macho64 ft_write.s && nasm -f macho64 ft_read.s && ...
+**		gcc main.c -c
+**		gcc main.o ft_strlen.o ft_write.o ft_read.o
+**		./a.out
 */
-#include <stdio.h>
-int ft_strlen(char *str);
-ssize_t ft_write(int fd, char * cbuf, ssize_t nbyte);
-ssize_t ft_read(int fd, char * cbuf, ssize_t nbyte);
+
+size_t ft_strlen(char *str);
+size_t ft_write(int fd, char *cbuf, size_t nbyte);
+size_t ft_read(int fd, char *cbuf, size_t nbyte);
 
 int main()
 {
-	// FT_STRLEN
-	
-	char *sopita = "sopita\n";
-	int len =ft_strlen(sopita);
-	printf("Len = %d\n", len);
+	// ft_strlen
+	char *string = "Prueba de write";
+	size_t len = ft_strlen(string);
+	printf("ft_strlen\nString=%s|Len=%zu\n", string, len);
 	fflush(stdout);
 
 	// FT_WRITE
-	ft_write(1, sopita, len);
-	if(ft_write(-1, sopita, len) == -1)
+	ft_write(1, string, len);
+	if (ft_write(-1, string, len) == -1)
 	{
 		perror("Error");
 	}
 
-
 	//FT_WRITE
 	char buffer[15];
-	
-	int nbytes =ft_read(1,buffer,15);
+
+	int nbytes = ft_read(1, buffer, 15);
 
 	printf("N Bytes del read = %d || STRING = %s\n", nbytes, buffer);
-	fflush(stdout);
+	//fflush(stdout);
 	nbytes = ft_read(-1, buffer, 15);
-	if(nbytes == -1)
+	if (nbytes == -1)
 	{
 		perror("Error");
 	}
