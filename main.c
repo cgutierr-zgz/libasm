03bcfcb91a99d91ec9ae70e4a73d5163dff41a90
Carlos <carlosgutimo.3d@gmail.com>
2021-04-28 22:14:59 +0200

    28/04/2021 22:14

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cgutierr <cgutierr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 19:03:36 by cgutierr          #+#    #+#             */
-/*   Updated: 2021/04/28 19:45:57 by cgutierr         ###   ########.fr       */
+/*   Updated: 2021/04/28 22:14:36 by cgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,23 @@ gcc main.c -c
 cc main.o ft_strlen.o
 ./a.out
 
+nasm -f macho64 a todas las funciones
 nasm -f macho64 ft_write.s && gcc main.c -c && gcc main.o ft_strlen.o ft_write.o && ./a.out
 */
 #include <stdio.h>
 int ft_strlen(char *str);
 ssize_t ft_write(int fd, char * cbuf, ssize_t nbyte);
-ssize_t read(int fd, char * cbuf, ssize_t nbyte);
-
-
-
+ssize_t ft_read(int fd, char * cbuf, ssize_t nbyte);
 
 int main()
 {
 	// FT_STRLEN
+	
 	char *sopita = "sopita\n";
 	int len =ft_strlen(sopita);
 	printf("Len = %d\n", len);
 	fflush(stdout);
+
 	// FT_WRITE
 	ft_write(1, sopita, len);
 	if(ft_write(-1, sopita, len) == -1)
@@ -45,9 +45,15 @@ int main()
 	//FT_WRITE
 	char buffer[15];
 	
-	int nbytes =read(1,buffer,15);
+	int nbytes =ft_read(1,buffer,15);
 
 	printf("N Bytes del read = %d || STRING = %s\n", nbytes, buffer);
 	fflush(stdout);
+	nbytes = ft_read(-1, buffer, 15);
+	if(nbytes == -1)
+	{
+		perror("Error");
+	}
+
 	return 0;
 }
