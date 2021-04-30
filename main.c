a8ead30321724de71bd6932528544d1e500bb4f1
Carlos <carlosgutimo.3d@gmail.com>
2021-04-30 14:01:40 +0200

    30/04/2021 14:01

--- main.c
+++ main.c
@@ -6,18 +6,20 @@
 /*   By: cgutierr <cgutierr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 19:03:36 by cgutierr          #+#    #+#             */
-/*   Updated: 2021/04/29 18:34:38 by cgutierr         ###   ########.fr       */
+/*   Updated: 2021/04/30 14:01:35 by cgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 /*
 ** Compilar:
 **	nasm -f macho64 ft_strlen.s && nasm -f macho64 ft_strcpy.s && nasm -f macho64 ft_strcmp.s && nasm -f macho64 ft_write.s && nasm -f macho64 ft_read.s && nasm -f macho64 ft_strdup.s
 **	gcc main.c -c
 **	gcc main.o ft_strlen.o ft_strcpy.o ft_strcmp.o ft_write.o ft_read.o ft_strdup.o
+**	./a.out
 */
 
 size_t	ft_strlen(const char *s);
@@ -28,19 +30,35 @@ size_t	ft_read(int fd, char *cbuf, size_t nbyte);
 char	*ft_strdup(const char *s1);
 
 int	main()
-{ //TODO: meter originales
-	// ft_strlen
+{
+	// FT_STRLEN
 	char *string = "Prueba de write";
 	size_t len = ft_strlen(string);
 	printf("ft_strlen\nString=%s|Len=%zu\n", string, len);
+	len = strlen(string);
+	printf("\nstrlen\nString=%s|Len=%zu\n", string, len);
 	fflush(stdout);
 
 	// FT_WRITE
+	printf("\nft_write\n");
+	fflush(stdout);
 	ft_write(1, string, len);
+	printf("\n");
+	fflush(stdout);
 	if (ft_write(-1, string, len) == -1)
-	{
 		perror("Error");
-	}
+	printf("\n");
+	fflush(stdout);
+
+	printf("write\n");
+	fflush(stdout);
+	write(1, string, len);
+	printf("\n");
+	fflush(stdout);
+	if (ft_write(-1, string, len) == -1)
+		perror("Error");
+	printf("\n");
+	fflush(stdout);
 
 	//FT_STRCPY
 	char *o_str1 = strdup("sopa");
