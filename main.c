63cd8bf4f2c20a2b6ae5253807d9fbcdd0969889
Carlos <carlosgutimo.3d@gmail.com>
2021-04-30 14:33:43 +0200

    30/04/2021 14:33

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cgutierr <cgutierr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 19:03:36 by cgutierr          #+#    #+#             */
-/*   Updated: 2021/04/30 14:01:35 by cgutierr         ###   ########.fr       */
+/*   Updated: 2021/04/30 14:32:00 by cgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ int	main()
 	char *string = "Prueba de write";
 	size_t len = ft_strlen(string);
 	printf("ft_strlen\nString=%s|Len=%zu\n", string, len);
+	// STRLEN
 	len = strlen(string);
 	printf("\nstrlen\nString=%s|Len=%zu\n", string, len);
 	fflush(stdout);
@@ -49,7 +50,7 @@ int	main()
 		perror("Error");
 	printf("\n");
 	fflush(stdout);
-
+	// WRITE
 	printf("write\n");
 	fflush(stdout);
 	write(1, string, len);
@@ -60,32 +61,36 @@ int	main()
 	printf("\n");
 	fflush(stdout);
 
-	//FT_STRCPY
+	// FT_STRCPY
+	char *str1 = strdup("sopa");
+	char *str2 = strdup("sopita");
+	printf("Antes=[%s]\n", str2);
+	printf("ft_Strcpy=[%s]\n", ft_strcpy(str2, str1));
+	// STRCPY
 	char *o_str1 = strdup("sopa");
 	char *o_str2 = strdup("sopita");
 	printf("Antes=[%s]\n", o_str2);
 	printf("Strcpy=[%s]\n", strcpy(o_str2, o_str1));
 
-	char *str1 = strdup("sopa");
-	char *str2 = strdup("sopita");
-	printf("Antes=[%s]\n", str2);
-	printf("ft_Strcpy=[%s]\n", ft_strcpy(str2, str1));
 
 	//FT_STRCMP
-	printf("strcmp=%d\n", strcmp("Soph", "Sopa"));
 	printf("ft_strcmp=%d\n", ft_strcmp("A", "a"));
+	// STRCMP
+	printf("strcmp=%d\n", strcmp("Soph", "Sopa"));
 
-	//FT_WREAD
+	//FT_READ
+	char buffer2[15];
+	int nbytes2 = ft_read(1, buffer2, 15);
+	printf("N Bytes del ft_read = %d || STRING = %s\n", nbytes2, buffer2);
+	nbytes2 = ft_read(-1, buffer2, 15);
+	if (nbytes2 == -1)
+		perror("Error");
+	// READ
 	char buffer[15];
-
-	int nbytes = ft_read(1, buffer, 15);
-
+	int nbytes = read(1, buffer, 15);
 	printf("N Bytes del read = %d || STRING = %s\n", nbytes, buffer);
-	//fflush(stdout);
 	nbytes = ft_read(-1, buffer, 15);
 	if (nbytes == -1)
-	{
 		perror("Error");
-	}
 	return 0;
 }
