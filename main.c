b7e85f6504064127bb9ca7b52a12d95dca7511b6
Carlos <carlosgutimo.3d@gmail.com>
2021-04-30 14:56:53 +0200

    30/04/2021 14:56

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cgutierr <cgutierr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 19:03:36 by cgutierr          #+#    #+#             */
-/*   Updated: 2021/04/30 14:32:00 by cgutierr         ###   ########.fr       */
+/*   Updated: 2021/04/30 14:56:35 by cgutierr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,24 +34,23 @@ int	main()
 	// FT_STRLEN
 	char *string = "Prueba de write";
 	size_t len = ft_strlen(string);
-	printf("ft_strlen\nString=%s|Len=%zu\n", string, len);
+	printf("**ft_strlen**\nString=%s|Len=%zu\n", string, len);
 	// STRLEN
 	len = strlen(string);
-	printf("\nstrlen\nString=%s|Len=%zu\n", string, len);
+	printf("**strlen**\nString=%s|Len=%zu\n", string, len);
 	fflush(stdout);
 
 	// FT_WRITE
-	printf("\nft_write\n");
+	printf("\n**ft_write**\n");
 	fflush(stdout);
 	ft_write(1, string, len);
 	printf("\n");
 	fflush(stdout);
 	if (ft_write(-1, string, len) == -1)
 		perror("Error");
-	printf("\n");
 	fflush(stdout);
 	// WRITE
-	printf("write\n");
+	printf("**write**\n");
 	fflush(stdout);
 	write(1, string, len);
 	printf("\n");
@@ -64,33 +63,43 @@ int	main()
 	// FT_STRCPY
 	char *str1 = strdup("sopa");
 	char *str2 = strdup("sopita");
+	printf("**ft_strcpy**\n");
 	printf("Antes=[%s]\n", str2);
-	printf("ft_Strcpy=[%s]\n", ft_strcpy(str2, str1));
+	printf("Después=[%s]\n", ft_strcpy(str2, str1));
 	// STRCPY
 	char *o_str1 = strdup("sopa");
 	char *o_str2 = strdup("sopita");
+	printf("**strcpy**\n");
 	printf("Antes=[%s]\n", o_str2);
-	printf("Strcpy=[%s]\n", strcpy(o_str2, o_str1));
+	printf("Después=[%s]\n", strcpy(o_str2, o_str1));
 
 
 	//FT_STRCMP
-	printf("ft_strcmp=%d\n", ft_strcmp("A", "a"));
+	char *str1234 = "sopa";
+	char *str234 = "sopato";
+	printf("\n**ft_strcmp**\n%s,%s = %d\n", str1234, str234, ft_strcmp(str1234, str234));
 	// STRCMP
-	printf("strcmp=%d\n", strcmp("Soph", "Sopa"));
+	printf("**strcmp**\n%s,%s = %d\n\n", str1234, str234, strcmp(str1234, str234));
 
 	//FT_READ
 	char buffer2[15];
+	printf("**ft_read**\n");
+	fflush(stdout);
 	int nbytes2 = ft_read(1, buffer2, 15);
-	printf("N Bytes del ft_read = %d || STRING = %s\n", nbytes2, buffer2);
+	printf("N Bytes = %d || STRING = %s\n", nbytes2, buffer2);
+	fflush(stdout);
 	nbytes2 = ft_read(-1, buffer2, 15);
 	if (nbytes2 == -1)
-		perror("Error");
+		perror("Error ft_read");
 	// READ
 	char buffer[15];
+	printf("**read**\n");
+	fflush(stdout);
 	int nbytes = read(1, buffer, 15);
-	printf("N Bytes del read = %d || STRING = %s\n", nbytes, buffer);
-	nbytes = ft_read(-1, buffer, 15);
+	printf("N Bytes = %d || STRING = %s\n", nbytes, buffer);
+	fflush(stdout);
+	nbytes = read(-1, buffer, 15);
 	if (nbytes == -1)
-		perror("Error");
+		perror("Error read");
 	return 0;
 }
